6e1697cb064fbb8ef64b438397420c949281171e
kurokuman <klg.pkt0812@gmail.com>
2020-02-19 21:59:34 +0900

    docker setup

---
+++ docker/mysql/initdb.d/testdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name,email) VALUES ('TOM','xxxx@mail.co.jp');
+INSERT INTO users (name,email) VALUES ('か','yyy@mail.co.jp');
+INSERT INTO users (name,email) VALUES ('日本語','dfjks@mail.co.jp');
