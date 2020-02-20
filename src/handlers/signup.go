302914fff1fff175c0dcdb9581ce1df76430f947
kurokuman <klg.pkt0812@gmail.com>
2020-02-20 21:22:24 +0900

    create signupform and get method function

--- src/handlers/signup.go
+++ src/handlers/signup.go
@@ -5,8 +5,15 @@ import (
 	"net/http"
 )
 
-func DisplaySignUpForm(w http.ResponseWriter, r *http.Request) {
+type SignUpForm struct {
+	FieldNames []string
+	Fileds     map[string]string
+	Errors     map[string]string
+}
+
+func DisplaySignUpForm(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
 	fmt.Println("get")
+	RenderTemplate(w, "./templates/signupform.html", s)
 }
 
 func ValidateSignUpForm(w http.ResponseWriter, r *http.Request) {
@@ -16,14 +23,19 @@ func ValidateSignUpForm(w http.ResponseWriter, r *http.Request) {
 
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
 
+	s := SignUpForm{}
+	s.FieldNames = []string{"username", "email"}
+	s.Fileds = make(map[string]string)
+	s.Errors = make(map[string]string)
+
 	switch r.Method {
 
 	case "GET":
-		DisplaySignUpForm(w, r)
+		DisplaySignUpForm(w, r, &s)
 	case "POST":
 		ValidateSignUpForm(w, r)
 	default:
-		DisplaySignUpForm(w, r)
+		DisplaySignUpForm(w, r, &s)
 	}
 
 }
