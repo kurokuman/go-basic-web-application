fb85953d8adfd00374d62b4378363076e23d1a06
kurokuman <klg.pkt0812@gmail.com>
2020-02-20 21:50:38 +0900

    implement post sign up form

--- src/handlers/signup.go
+++ src/handlers/signup.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"unicode/utf8"
 )
 
 type SignUpForm struct {
@@ -16,8 +17,30 @@ func DisplaySignUpForm(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
 	RenderTemplate(w, "./templates/signupform.html", s)
 }
 
-func ValidateSignUpForm(w http.ResponseWriter, r *http.Request) {
+func ValidateSignUpForm(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
 	fmt.Println("post")
+	if utf8.RuneCountInString(r.FormValue("username")) <= 8 {
+		s.Errors["usernameError"] = "The name must be at least eight characters long."
+	}
+
+	if r.FormValue("email") == "" {
+		s.Errors["emailError"] = "The e-mail address field is required"
+	}
+
+	if r.FormValue("pass") != r.FormValue("c_pass") {
+		s.Errors["confirmPasswordError"] = "The password and confirm password don't match"
+	}
+
+	if len(s.Errors) > 0 {
+		DisplaySignUpForm(w, r, s)
+	} else {
+		ProcessSignUpForm(w, r, s)
+	}
+
+}
+
+func ProcessSignUpForm(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
+	DisplaySignUpForm(w, r, s)
 
 }
 
@@ -33,7 +56,7 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		DisplaySignUpForm(w, r, &s)
 	case "POST":
-		ValidateSignUpForm(w, r)
+		ValidateSignUpForm(w, r, &s)
 	default:
 		DisplaySignUpForm(w, r, &s)
 	}
